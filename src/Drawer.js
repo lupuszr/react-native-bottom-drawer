eb4921e6fc83d7b07e11be1264ba3f1b36060581
Viktor Pelle <vikpelle@gmail.com>
2018-03-30 11:51:58 +0200

    Update zIndex

--- src/Drawer.js
+++ src/Drawer.js
@@ -347,7 +347,7 @@ export default class Drawer extends Component {
   getContainerStyle = () => ({
     // Move the view below others if not open or moving
     // to not block gesture handlers on other views
-    zIndex: this.state.pulling || this.state.open ? 1 : -1,
+    zIndex: this.state.pulling || this.state.open ? 999 : -1,
   });
 }
 
